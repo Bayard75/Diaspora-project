097f9056ac2e38b180dba62f7fffebeb08da899e
Bayard75 <bayard.maniraho@gmail.com>
2020-10-28 00:39:22 +0100

    front almosted finished added cards, checkboxes, post requests, and csv download

---
+++ app.py
@@ -0,0 +1,124 @@
+import requests
+from urllib.parse import quote
+from flask import Flask, render_template, request, jsonify, make_response
+from datetime import datetime
+
+import pandas as pd
+
+app = Flask(__name__)
+TOKEN = "EAABsbCS1iHgBAHTmw316FuXn1YmFCwT5qZCy7ZAG3AuADr7yCW861uTQzYt5VPdj76EOrBi5GwXcH5ta9nfkhwUZB659YL7Eg8jtavWZAmT9FJFkIYsPKAjRKME4q3W6lKQ3cYAwCy4tiCv69OzrBRaQnHOu5ngDzygkiZBcGrgZDZD"
+    
+
+def get_behaviors():
+    behaviors = [{
+                    "id": "6023422105983",
+                    "name": "A vécu en Côte d’Ivoire (anciennement Expats - Côte d’Ivoire)",
+                    "short_name": "Côte d'Ivoire"
+                },
+                {
+                    "id":"6023357000583",
+                    "name": "A vécu au Sénégal (anciennement Expats - Sénégal)",
+                    "short_name": "Sénégal"
+                }]
+    return behaviors
+
+
+def get_countries():
+    countries = [{
+        "code": "FR",
+        "name": "France"
+    },{
+        "code": "GB",
+        "name": "Royaume-Uni"
+    },{
+        "code": "IT",
+        "name": "Italie"
+    },{
+        "code": "ES",
+        "name": "Espagne"
+    },{
+        "code": "CA",
+        "name": "Canada"
+    },{
+        "code": "US",
+        "name": "Etats-Unis"
+    },{
+        "code": "SN",
+        "name": "Sénégal"
+    },{
+        "code": "CI",
+        "name": "Côte d'Ivoire"
+    },{
+        "code": "BE",
+        "name": "Belgique"
+    },{
+        "code": "CH",
+        "name": "Suisse"
+    },{
+        "code": "DE",
+        "name": "Allemagne"
+    },{
+        "code": "PT",
+        "name": "Portugal"
+    },{
+        "code": "BF",
+        "name": "Burkina Faso"
+    },{
+        "code": "GN",
+        "name": "Guinee"
+    },{
+        "code": "MA",
+        "name": "Maroc"
+    },{
+        "code": "GA",
+        "name": "Gabon"
+    }]
+    return countries
+
+
+def get_estimate(gender, residence_code, origine_id, origine_name, age_max=65, age_min=13):
+    targeting_spec = '{"genders":[%s],"age_max":%s,"age_min":%s,"geo_locations":{"countries":["%s"],"location_types":["home","recent"]},"flexible_spec":[{"behaviors":[{"id":"%s","name":"%s"}]}]}' % (gender, age_max, age_min, residence_code, origine_id, origine_name)
+    params = quote(targeting_spec, safe='')
+    url = f'https://graph.facebook.com/v7.0/act_1047742262329813/reachestimate?access_token={TOKEN}&__activeScenarioIDs=[]&__activeScenarios=[]&_app=ADS_MANAGER&_index=163&_reqName=adaccount%2Freachestimate&_reqSrc=AdsTargetingEstimatedReach.react&_sessionID=377669edb48eb987&include_headers=false&locale=fr_FR&method=get&pretty=0&suppress_http_code=1&targeting_spec={params}&xref=f2fd6fe30bbdf3'
+    res = requests.get(url)
+    return res.json()['data']['users']
+
+@app.route("/")
+@app.route("/home",  methods=['POST'])
+def home():
+    if request.method == 'POST':
+        residence_wanted = request.get_json()["residence"]
+        origine_wanted = request.get_json()["origine"]
+        path = generate_csv(residence_wanted, origine_wanted)
+        return make_response(jsonify({'status': 'ok', "path": path}))
+
+    behaviors = get_behaviors()
+    countries = get_countries()
+    return render_template('index.html', behaviors=behaviors, countries=countries)
+
+
+def generate_csv(residence_wanted, origine_wanted, age_max="65", age_min="13"):
+    df = pd.DataFrame(columns=['Pays de residence', "Pays d'origine", "Age Minimum", "Age Maximum", "Nombre d'utilisateur Total", "Hommes", "Femmes"])
+    for residence in residence_wanted:
+        for origine in origine_wanted:
+            for gender in range(0,3):
+                if gender == 0:
+                    users_total = get_estimate(gender, residence['id'], origine['id'], origine["name"])
+                elif gender == 1:
+                    male_users = get_estimate(gender, residence['id'], origine['id'], origine["name"])
+                elif gender == 2:
+                    femele_users = get_estimate(gender, residence['id'], origine['id'], origine["name"])
+                    new_row = {"Pays de residence": residence["name"],
+                                "Pays d'origine": origine["short_name"],
+                                "Age Minimum": age_min, "Age Maximum": age_max,
+                                "Nombre d'utilisateur Total" : users_total, 
+                                "Hommes": male_users,  "Femmes": femele_users}
+                    df = df.append(new_row, ignore_index=True)
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
+
+    df.to_csv(f'static/assets/Diaspora_{dt_string}.csv')
+    return f'static/assets/Diaspora_{dt_string}.csv'
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- facebook.py
+++ facebook.py
@@ -1,24 +1,6 @@
-import requests
-import json
-from urllib.parse import quote
+import pandas as pd
 
-token = "EAABsbCS1iHgBAHTmw316FuXn1YmFCwT5qZCy7ZAG3AuADr7yCW861uTQzYt5VPdj76EOrBi5GwXcH5ta9nfkhwUZB659YL7Eg8jtavWZAmT9FJFkIYsPKAjRKME4q3W6lKQ3cYAwCy4tiCv69OzrBRaQnHOu5ngDzygkiZBcGrgZDZD"
-
-behavior_requets = requests.get('https://graph.facebook.com/v7.0/act_1047742262329813/targetingsuggestions?access_token=EAABsbCS1iHgBAHTmw316FuXn1YmFCwT5qZCy7ZAG3AuADr7yCW861uTQzYt5VPdj76EOrBi5GwXcH5ta9nfkhwUZB659YL7Eg8jtavWZAmT9FJFkIYsPKAjRKME4q3W6lKQ3cYAwCy4tiCv69OzrBRaQnHOu5ngDzygkiZBcGrgZDZD&_app=ADS_MANAGER&_index=36&_reqName=adaccount%2Ftargetingsuggestions&_reqSrc=AdsUnifiedSuggestionsDataSource&_sessionID=7f5b870e4df0a349&countries=%5B%22FR%22%5D&include_headers=false&locale=fr_FR&method=get&pretty=0&regulated_categories=%5B%5D&session_id=1409807087955&suppress_http_code=1&targeting_list=%5B%7B%22id%22%3A%226019564340583%22%2C%22type%22%3A%22behaviors%22%7D%5D&xref=f353c77137ae35')
-expats_options = behavior_requets.json()
-
-for i in expats_options['data']:
-    print(f'name : {i["name"]}, id : {i["id"]}')
-print('choose number ')
-choice = input()
-chosen = expats_options['data'][int(choice)+1]
-targeting_spec= '{"genders":[0],"age_max":65,"age_min":45,"geo_locations":{"countries":["FR"],"location_types":["home","recent"]},"flexible_spec":[{"behaviors":[{"id":"%s","name":"%s"}]}]}' % (chosen['id'], chosen['name'])
-
-params = quote(targeting_spec, safe='')
-
-
-url = f'https://graph.facebook.com/v7.0/act_1047742262329813/reachestimate?access_token={token}&__activeScenarioIDs=[]&__activeScenarios=[]&_app=ADS_MANAGER&_index=163&_reqName=adaccount%2Freachestimate&_reqSrc=AdsTargetingEstimatedReach.react&_sessionID=377669edb48eb987&include_headers=false&locale=fr_FR&method=get&pretty=0&suppress_http_code=1&targeting_spec={params}&xref=f2fd6fe30bbdf3'
-res = requests.get(url)
-
-countrys = requests.get(f'https://graph.facebook.com/v2.11/search?type=adgeolocation&location_types=["country"]&access_token={token}')
-
+df = pd.DataFrame(columns=['Pays de residence', "Pays d'origine", "Age Minimum", "Age Maximum", "Nombre d'utilisateur Total", "Hommes", "Femmes"])
+new_row = {"Pays de residence":"a", "Pays d'origine":"D", "Age Minimum":"f","Age Maximum":"F","Nombre d'utilisateur Total":"3", "Hommes":'5', "Femmes":"5"}
+df = df.append(new_row, ignore_index=True)
+print(df)
